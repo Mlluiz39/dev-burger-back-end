c8065af9337658eedfcf17bcc2fa0dc443baddf1
Marcelo Luiz <mlluizpereira39@gmail.com>
2023-02-22 12:14:05 -0300

    add cors

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const routes = require("./routes");
 const resolve = require("path").resolve;
+const cors = require("cors");
 
 require("./database");
 
 class App {
   constructor() {
     this.app = express();
+    this.app.use(cors());
     this.middleware();
     this.routes();
   }
