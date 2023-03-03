41920627f4235cfbf376724a91c4bb52bb70cd72
Marcelo Luiz <mlluizpereira39@gmail.com>
2023-03-03 12:11:59 -0300

    altered status code in users conflict

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -23,7 +23,7 @@ class UserController {
     const userExists = await User.findOne({ where: { email } });
 
     if (userExists) {
-      return res.status(400).json({ error: "User already exists" });
+      return res.status(409).json({ error: "User already exists" });
     }
 
     const user = await User.create({
